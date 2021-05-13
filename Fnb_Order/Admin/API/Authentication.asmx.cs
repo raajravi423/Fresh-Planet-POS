7af34ebda01ba2f44ad583224a298064f0057ca7
manishkumar2080 <manishkumar2080@gmail.com>
2021-05-13 21:41:22 +0530

    ASMX Files and Deleted WCF svc files.

---
+++ Fnb_Order/Admin/API/Authentication.asmx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Services;
+using System.Web.Services;
+
+namespace Fnb_Order.Admin.API
+{
+    /// <summary>
+    /// Summary description for Authentication
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class Authentication : System.Web.Services.WebService
+    {
+
+        [WebMethod, ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public string getStudents()
+        {
+            List<Student> stuList = new List<Student>();
+            stuList.Add(new Student { ID = "123", Name = "Ravi Katiyar" });
+           // return JsonConvert.SerializeObject(stuList);
+            System.Web.Script.Serialization.JavaScriptSerializer js = new System.Web.Script.Serialization.JavaScriptSerializer();
+
+            return js.Serialize(stuList);
+
+            //   return stuList;
+        }
+    }
+}
