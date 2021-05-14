cc49bc72c0e65b78bbdae76f4232e08bd2b60224
manishkumar2080 <manishkumar2080@gmail.com>
2021-05-14 21:32:17 +0530

    Authentication Service added

--- Fnb_Order/Admin/API/Authentication.asmx.cs
+++ Fnb_Order/Admin/API/Authentication.asmx.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Script.Services;
 using System.Web.Services;
@@ -29,6 +32,33 @@ namespace Fnb_Order.Admin.API
             return js.Serialize(stuList);
 
             //   return stuList;
+        }
+
+        [WebMethod, ScriptMethod(ResponseFormat = ResponseFormat.Json, UseHttpGet = true)]
+        public HttpStatusCode GetUserAuthentication(string UserName, string UserPassword)
+        {
+            DataTable fetchedrecords = new DataTable();
+
+            SqlConnection oConn = new SqlConnection(GlobalPath.ConnectionString);
+            SqlCommand oComm = new SqlCommand("select * from tbl_Users where UserID = '" + UserName + "' And Password = '" + UserPassword + "' and Status ='1'", oConn);
+            SqlDataAdapter da = new SqlDataAdapter(oComm);
+            oConn.Open();
+            da.Fill(fetchedrecords);
+            oConn.Close();
+            if (fetchedrecords.Rows.Count > 0)
+            {
+                SessionHandler.UserID = fetchedrecords.Rows[0]["UserID"].ToString();
+                SessionHandler.UserName = fetchedrecords.Rows[0]["UserName"].ToString();
+                SessionHandler.UserRole = fetchedrecords.Rows[0]["UserRole"].ToString();
+
+                return HttpStatusCode.OK;
+               // Response.Redirect("/Admin/UploadRateList.aspx");
+            }
+            else
+            {
+                return HttpStatusCode.Found;
+ 
+            }
         }
     }
 
